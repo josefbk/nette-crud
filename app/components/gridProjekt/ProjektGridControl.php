302495fbb348740bbfd8766837469599f5897139
Josef <Josefbk@gmail.com>
2017-08-20 22:37:13 +0200

    preklad pro grid

--- app/components/gridProjekt/ProjektGridControl.php
+++ app/components/gridProjekt/ProjektGridControl.php
@@ -59,6 +59,28 @@ class ProjektGridControl extends Control
                 return 'Opravdu odstranit projekt ID ' . $item[Projekt::COLUMN_ID] . ' - %s?';
             }, Projekt::COLUMN_NAZEV_PROJEKTU);
 
+        /**
+         * Localization
+         */
+        $translator = new \Ublaboo\DataGrid\Localization\SimpleTranslator([
+            'ublaboo_datagrid.no_item_found_reset' => 'Žádné položky nenalezeny. Filtr můžete vynulovat',
+            'ublaboo_datagrid.no_item_found' => 'Žádné položky nenalezeny.',
+            'ublaboo_datagrid.here' => 'zde',
+            'ublaboo_datagrid.items' => 'Položky',
+            'ublaboo_datagrid.all' => 'všechny',
+            'ublaboo_datagrid.from' => 'z',
+            'ublaboo_datagrid.reset_filter' => 'Resetovat filtr',
+            'ublaboo_datagrid.group_actions' => 'Hromadné akce',
+            'ublaboo_datagrid.show_all_columns' => 'Zobrazit všechny sloupce',
+            'ublaboo_datagrid.hide_column' => 'Skrýt sloupec',
+            'ublaboo_datagrid.action' => 'Akce',
+            'ublaboo_datagrid.previous' => 'Předchozí',
+            'ublaboo_datagrid.next' => 'Další',
+            'ublaboo_datagrid.choose' => 'Vyberte',
+            'ublaboo_datagrid.execute' => 'Provést',
+        ]);
+        $grid->setTranslator($translator);
+
         return $grid;
 
     }
